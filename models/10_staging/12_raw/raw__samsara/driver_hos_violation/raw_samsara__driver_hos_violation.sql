beef6b0ec006f19a12e79b23a1a4fe428078dcd0
David Friedman <developer@dtechnician.com>
2025-12-09 19:57:49 +0000

    fix driver hos violation incremental filter col

--- models/10_staging/12_raw/raw__samsara/driver_hos_violation/raw_samsara__driver_hos_violation.sql
+++ models/10_staging/12_raw/raw__samsara/driver_hos_violation/raw_samsara__driver_hos_violation.sql
@@ -6,6 +6,6 @@
     source_name='samsara',
     table_name='driver_hos_violation',
     column_expressions=columns,
-    incremental_filter_column='updated_at_time',
+    incremental_filter_column='_FIVETRAN_SYNCED',
     unique_key=['driver_id','day_start_time','type']
 ) }}

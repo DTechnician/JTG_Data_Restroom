ab86dd2daae55df9e550526c43e9fa0d36d0d1e8
David Friedman <developer@dtechnician.com>
2026-03-12 18:50:04 +0000

    dim workorde SK's

--- models/20_intermediate/21_dims/dim_work_order/dim_work_order.sql
+++ models/20_intermediate/21_dims/dim_work_order/dim_work_order.sql
@@ -12,21 +12,26 @@
     natural_key     = ['workordernumber'],
     attributes = [
 		"account_id",
-        "contact_id",
-        "driver_id",
-        "exception_reason_code",
-        "material_type_id",
-        "posted_status_name",
-        "route_id",
-        "serviceregion_id",
         "siteservice_id",
-        "status",
-        "truck_id",
+        "site_id",
+        "servicecode_id",
+        "equipment_type_id",
+        "lob_id",
         "work_type_id",
+        "route_id",
+        "driver_id",
+        "truck_id",
+
+        "serviceregion_id",
+        "contact_id",
+        "material_type_id",
 
         "scheduled_date",
         "completion_date",
 
+        "exception_reason_code",
+        "posted_status_name",
+        "status",
         "quantity"
 	],
     derived_attributes = [
@@ -46,6 +51,89 @@
             ]
         },
         {
+            'name': 'site_service_history_sk',
+            'dim_model': 'dim_site_service_history',
+            'dim_sk': 'site_service_history_sk',
+            'join_type': 'left',
+            'join_on': [
+                {'src': 'siteservice_id', 'dim': 'service_id'}
+            ]
+        },
+        {
+            'name': 'site_sk',
+            'dim_model': 'dim_site',
+            'dim_sk': 'site_sk',
+            'join_type': 'left',
+            'join_on': [
+                {'src': 'site_id', 'dim': 'site_id'}
+            ]
+        },
+        {
+            'name': 'service_code_sk',
+            'dim_model': 'dim_service_code',
+            'dim_sk': 'service_code_sk',
+            'join_type': 'left',
+            'join_on': [
+                {'src': 'servicecode_id', 'dim': 'service_code_id'}
+            ]
+        },
+        {
+            'name': 'equipment_type_sk',
+            'dim_model': 'dim_equipment_type',
+            'dim_sk': 'equipment_type_sk',
+            'join_type': 'left',
+            'join_on': [
+                {'src': 'equipment_type_id', 'dim': 'equipment_type_id'}
+            ]
+        },
+        {
+            'name': 'lob_sk',
+            'dim_model': 'dim_line_of_business',
+            'dim_sk': 'lob_sk',
+            'join_type': 'left',
+            'join_on': [
+                {'src': 'lob_id', 'dim': 'lob_id'}
+            ]
+        },
+        {
+            'name': 'work_type_sk',
+            'dim_model': 'dim_work_type',
+            'dim_sk': 'work_type_sk',
+            'join_type': 'left',
+            'join_on': [
+                {'src': 'work_type_id', 'dim': 'work_type_id'}
+            ]
+        },
+        {
+            'name': 'route_sk',
+            'dim_model': 'dim_route',
+            'dim_sk': 'route_sk',
+            'join_type': 'left',
+            'join_on': [
+                {'src': 'route_id', 'dim': 'route_id'}
+            ]
+        },
+        {
+            'name': 'driver_sk',
+            'dim_model': 'dim_driver_navusoft',
+            'dim_sk': 'driver_sk',
+            'join_type': 'left',
+            'join_on': [
+                {'src': 'driver_id', 'dim': 'driver_id'}
+            ]
+        },
+        {
+            'name': 'vehicle_sk',
+            'dim_model': 'dim_vehicle_navusoft',
+            'dim_sk': 'vehicle_sk',
+            'join_type': 'left',
+            'join_on': [
+                {'src': 'truck_id', 'dim': 'truck_id'}
+            ]
+        },
+
+        
+        {
             'name': 'scheduled_date_sk',
             'dim_model': 'dim_date',
             'dim_sk': 'date_sk',
@@ -61,15 +149,6 @@
             'join_type': 'left',
             'join_on': [
                 {'src': 'completion_date', 'dim': 'date'}
-            ]
-        },
-        {
-            'name': 'site_service_history_sk',
-            'dim_model': 'dim_site_service_history',
-            'dim_sk': 'site_service_history_sk',
-            'join_type': 'left',
-            'join_on': [
-                {'src': 'siteservice_id', 'dim': 'service_id'}
             ]
         }
     ],

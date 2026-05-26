545f03724863395c60ab7c2c692a9f9e236c4ae7
David Friedman <developer@dtechnician.com>
2026-05-26 20:18:35 +0000

    adding timecards table from custom connector sdk schema

---
+++ models/10_staging/12_raw/raw_adp/timecards/raw_adp__timecards.sql
@@ -0,0 +1,11 @@
+{% set columns = [
+    "*"
+] %}
+
+{{ generate_staging_model(
+    source_name='adp_custom',
+    table_name='timecards',
+    column_expressions=columns,
+    incremental_filter_column='_fivetran_synced',
+    unique_key= ['worker_id','timecard_id','date']
+) }}
